2e076bd0af82e47dde090770eaed5bf108814cd5
shunto756 <yamashun756@gmail.com>
2019-07-02 15:21:08 +0900

    Change code

--- webview.js
+++ webview.js
@@ -1,13 +1,7 @@
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
-  	//ブラウザのタイトルが更新されることを利用して通知をカウント(ver.1.0)
-	//var element = document.getElementsByClassName("badge");
-	//var text = document.title;
-	//var cut_a = text.split(")");
-	//var cut_b = cut_a[0].split("(");
-	//count = Number(cut_b[1]);
-	
-	//.badgeのdata-badge内に追加される数を利用してカウント(ver1.2)
+
+	//.badgeのdata-badge内に追加される数を利用してカウント
 	var elements = document.getElementsByClassName('badge');
     count = 0;
     for(i=0;i<elements.length;i++){
